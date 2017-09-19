f5f0358662024bcdaca1ad2b606a6122e3970ba9
Roger <floydmate@gmail.com>
2017-09-19 01:00:34 -0500

    ya realiza serie de nnumeros

--- practica3/pascal.c
+++ practica3/pascal.c
@@ -1,32 +1,39 @@
 #include <stdio.h>
 
+void initarray(int[],int);
+void printarr(int[],int);
 int main(){
-	int n,i;
-	printf("Introduzca numeros \n");
-	scanf("%d",&n);
-	int buffer[n+1];
-	int nums[n+1];
-	buffer[0]=1;
-	buffer[n]=1;
-	for(i = n;i>0;i--){
-		printf("i:%d",i);
-		int j;
-		for(j=i;j>0;j--)
-			printf("s");
-		nums[n]=1;
-		if(i<=n-2){
-			for(j = i+1;j<n;j++){	
-				nums[j]=buffer[i+1]+buffer[i-1];
-				printf("buff -1: %d",buffer[j-1]);
-				printf("buff +1: %d",buffer[j+1]);
-			}
-		}
-		buffer[n]=1;
-		for(j = i;j<n;j++){
-			buffer[j-1]=nums[j];
-			//printf("%d  ",nums[j]);	
-		}	
-		printf("\n");	
+    int n,i;
+    printf("Introduzca numeros \n");
+    scanf("%d",&n);
+    int buffer[n+1];
+    int nums[n+1];
+    buffer[n]=1;
+    initarray(buffer,n);
+    initarray(nums,n);
+    //printarr(buffer,n);
+    for(i = n;i>0;i--){
+	//Print spaces 
+	int j;
+	for(j=0;j<i;j++)
+	    printf(" ");
+	for(j=i;j<n;j++)
+	    nums[j]=buffer[j+1]+buffer[j];
+	for(j=i;j<n;j++){
+	    buffer[j]=nums[j];
+	    printf("%d ",nums[j]);
 	}
-	return 0;
+	printf("\n");
+    }
+    return 0;
+}
+
+void initarray(int arr[],int n){
+    while(n--!=0)
+	arr[n]=0;
+}
+void printarr(int arr[],int n){
+    int i = 0;
+    while(i<n)
+	printf("%d\n",arr[i++]);
 }
