b1ac76107bf6f1e816bc0f3e16af4d5fcc20bc26
Roger <floydmate@gmail.com>
2017-09-21 22:27:33 -0500

    realiza la serie correctamente

--- practica3/pascal.c
+++ practica3/pascal.c
@@ -3,37 +3,37 @@
 void initarray(int[],int);
 void printarr(int[],int);
 int main(){
-    int n,i;
-    printf("Introduzca numeros \n");
-    scanf("%d",&n);
+    int n,i,j;
+    n = 8;
+    //printf("Introduzca numeros \n");
+    //scanf("%d",&n);
     int buffer[n+1];
     int nums[n+1];
-    buffer[n]=1;
     initarray(buffer,n);
     initarray(nums,n);
-    //printarr(buffer,n);
-    for(i = n;i>0;i--){
-	//Print spaces 
-	int j;
+    buffer[n]=1;
+    for(i=n;i>=0;i--){
+	int k;
+	//print spaces
 	for(j=0;j<i;j++)
 	    printf(" ");
-	for(j=i;j<n;j++)
-	    nums[j]=buffer[j+1]+buffer[j];
 	for(j=i;j<n;j++){
-	    buffer[j]=nums[j];
-	    printf("%d ",nums[j]);
+	    k=buffer[j]+buffer[j+1];
+	    printf("%d  ",k);
+	    buffer[j]=k;
 	}
-	printf("\n");
+	printf("1\n");
     }
     return 0;
 }
 
 void initarray(int arr[],int n){
-    while(n--!=0)
-	arr[n]=0;
+    while(n>=0)
+	arr[n--]=0;
 }
 void printarr(int arr[],int n){
-    int i = 0;
-    while(i<n)
-	printf("%d\n",arr[i++]);
+    int i;
+    for(i=0;i<=n;i++)
+	printf("[%d]:%d",i,arr[i]);
+    printf("\n");
 }
